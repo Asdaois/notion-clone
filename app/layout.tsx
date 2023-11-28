6073b4d892699e2bcf2dc37564963dc09d08ca73
Jose Guevara <94gjose@gmail.com>
2023-11-28 10:30:14 -0400

    feat: add shadcn/ui
    Fixes 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import db, { migrateDb} from '@/src/lib/supabase/db'
+import db, { migrateDb} from '@/lib/supabase/db'
 
 migrateDb()
 
