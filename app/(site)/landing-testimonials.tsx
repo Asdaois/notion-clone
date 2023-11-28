4e6ca54396523ecea9b0af37f8ff909869ccb64c
Jose Guevara <94gjose@gmail.com>
2023-11-28 15:16:31 -0400

    feat: add testimonial section for landing page
    #4

--- app/(site)/landing-testimonials.tsx
+++ app/(site)/landing-testimonials.tsx
@@ -1,4 +1,11 @@
+import CustomCard from "@/components/landing-page/custom-card";
 import TitleSection from "@/components/landing-page/title-section";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CardDescription, CardTitle } from "@/components/ui/card";
+import { USERS } from "@/lib/constants";
+import clsx from "clsx";
+import { randomUUID } from "crypto";
+import { twMerge } from "tailwind-merge";
 
 const LandingTestimonials = ({}) => {
   return (
@@ -30,6 +37,55 @@ const LandingTestimonials = ({}) => {
             personal and professional productivity needs."
           pill="Testimonials"
         />
+        {[...Array(2)].map((arr, index) => {
+          return (
+            <div
+              key={randomUUID()}
+              className={twMerge(
+                clsx("mt-10 flex flex-nowrap gap-6 self-start", {
+                  "flex-row-reverse": index === 1,
+                  "animate-[slide_250s_linear_infinite]": true,
+                  "animate-[slide_250s_linear_infinite_reverse]": index === 1,
+                  "ml-[100vw]": index === 1,
+                }),
+                "hover:paused"
+              )}
+            >
+              {USERS.map((testimonial, index) => (
+                <CustomCard
+                  key={testimonial.message}
+                  className="w-[500px] 
+                  shrink-0 
+                  rounded-xl 
+                  dark:bg-gradient-to-t 
+                  dark:from-border 
+                  dark:to-background"
+                  cardHeader={
+                    <div className="flex items-center gap-4">
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div className="">
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLocaleLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }
+                />
+              ))}
+            </div>
+          );
+        })}
       </div>
     </section>
   );
