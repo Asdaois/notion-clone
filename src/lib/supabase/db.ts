13f4bf11be8a358d269c55af7f830052e9ab3a56
Jose Guevara <94gjose@gmail.com>
2023-11-28 09:40:55 -0400

    feat: Configure online .env vault
    run dummy db
    
    Fixes #2

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -12,10 +12,11 @@ if (!process.env.DATABASE_URL) {
 
 const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
 const db = drizzle(client, { schema });
-const migrateDb = async () => {
+export const migrateDb = async () => {
   try {
     console.log("Migrating client");
     await migrate(db, { migrationsFolder: "migrations" });
+    console.log("Migrating client");
   } catch (error) {
     console.log("Error migrating client");
   }
--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,1 +1,14 @@
-export { };
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const workspace = pgTable('workspaces', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }),
+  updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'string' }),
+  workspaceOwner: uuid('workspace_owner').notNull(),
+  title: text('title').notNull(),
+  iconID: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  logo: text('logo'),
+  bannerUrl: text('banner_url'),
+});
