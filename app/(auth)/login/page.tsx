d008148faf56b44a894c0102a280709fda3adad8
Jose Guevara <94gjose@gmail.com>
2023-11-29 11:39:35 -0400

    feat: add signup page
    Closed #10

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { JSXElementConstructor, ReactElement, useState } from "react";
-import {
-  ControllerFieldState,
-  ControllerRenderProps,
-  FieldValues,
-  SubmitHandler,
-  UseFormStateReturn,
-  useForm,
-} from "react-hook-form";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormSchema } from "@/lib/types";
-import { z } from "zod";
 import {
   Form,
   FormControl,
@@ -26,15 +19,14 @@ import Logo from "@/public/cypresslogo.svg";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Loader from "@/components/global/Loader";
+import { Separator } from "@/components/ui/separator";
 import { actionLoginUser } from "@/lib/server-action/auth-actions";
 
-type FormSchemaType = z.infer<typeof FormSchema>;
-
-const LoginPage = ({}) => {
+const LoginPage = () => {
   const router = useRouter();
-  const [submitError, setSubmitError] = useState<string>();
+  const [submitError, setSubmitError] = useState("");
 
-  const form = useForm<FormSchemaType>({
+  const form = useForm<z.infer<typeof FormSchema>>({
     mode: "onChange",
     resolver: zodResolver(FormSchema),
     defaultValues: { email: "", password: "" },
@@ -42,73 +34,87 @@ const LoginPage = ({}) => {
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit: SubmitHandler<FormSchemaType> = async (formData) => {
+  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+    formData
+  ) => {
     const { error } = await actionLoginUser(formData);
-    console.log("called?");
-
     if (error) {
       form.reset();
       setSubmitError(error.message);
     }
-
     router.replace("/dashboard");
   };
 
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
         onChange={() => {
           if (submitError) setSubmitError("");
         }}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
       >
-        <Link href={"/"} className="w-full flex justify-left items-center">
-          <Image src={Logo} alt="logo" width={50} height={50} />
-          <span className="font-semibold dark:text-white text-4xl ml-2">
-            Logo
+        <Link
+          href="/"
+          className="
+          w-full
+          flex
+          justify-left
+          items-center"
+        >
+          <Image src={Logo} alt="cypress Logo" width={50} height={50} />
+          <span
+            className="font-semibold
+          dark:text-white text-4xl first-letter:ml-2"
+          >
+            cypress.
           </span>
         </Link>
-        <FormDescription className="text-foreground/60">
-          An All-In-One Collaboration and Productivity Platform
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
         </FormDescription>
         <FormField
           disabled={isLoading}
           control={form.control}
           name="email"
-          render={(field) => (
+          render={({ field }) => (
             <FormItem>
               <FormControl>
                 <Input type="email" placeholder="Email" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
-        ></FormField>
+        />
         <FormField
           disabled={isLoading}
           control={form.control}
           name="password"
-          render={(field) => (
+          render={({ field }) => (
             <FormItem>
               <FormControl>
                 <Input type="password" placeholder="Password" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
-        ></FormField>
+        />
         {submitError && <FormMessage>{submitError}</FormMessage>}
         <Button
-          disabled={isLoading}
           type="submit"
           className="w-full p-6"
-          size={"lg"}
+          size="lg"
+          disabled={isLoading}
         >
           {!isLoading ? "Login" : <Loader />}
         </Button>
         <span className="self-container">
-          Don&apos;t have an account?{" "}
-          <Link href={"/signup"} className="text-primary">
-            Sing up
+          Don&#39;t have an account?{" "}
+          <Link href="/signup" className="text-primary">
+            Sign Up
           </Link>
         </span>
       </form>
