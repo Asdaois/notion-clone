70567eb5767e19be3d895be30bb65b05220d3ca0
Jose Guevara <94gjose@gmail.com>
2023-11-29 11:39:35 -0400

    feat: add signup page
    Closed #10

--- lib/server-action/auth-actions.ts
+++ lib/server-action/auth-actions.ts
@@ -1,8 +1,8 @@
 "use server";
 
-import { type FormSchemaType } from "../types";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { FormSchemaType } from "../types";
 
 export async function actionLoginUser({ email, password }: FormSchemaType) {
   const supabase = createRouteHandlerClient({ cookies });
@@ -12,5 +12,26 @@ export async function actionLoginUser({ email, password }: FormSchemaType) {
     password,
   });
 
+  return response;
+}
+
+export async function actionSignUpUser({
+  email,
+  password,
+}: FormSchemaType) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { data } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('email', email);
+
+  if (data?.length) return { error: { message: 'User already exists', data } };
+  const response = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}api/auth/callback`,
+    },
+  });
   return response;
 }
